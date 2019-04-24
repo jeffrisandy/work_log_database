67f519fafecc24caa7e8fea4758281755a1c3bdf
Jeffri Sandy <jeffri.sandy@gmail.com>
2019-04-24 15:30:59 +0700

    complete basic function code

--- work_log.py
+++ work_log.py
@@ -90,15 +90,17 @@ def view_entries(search_name=None,
                  search_time_spent=None,
                  search_term=None):
     """View previous entries"""
-    entries = Entry.select().order_by(Entry.timestamp.desc())
+
+    entries = Entry.select(Entry.id).order_by(Entry.timestamp.desc())
 
     if search_name:
         name = "%" + search_name + "%"
         entries = entries.where(Entry.name ** name)
 
-
     elif search_date:
-        entries = entries.where(Entry.timestamp == search_date)
+        entries = entries.where( (Entry.timestamp.year == search_date.year) &
+                                 (Entry.timestamp.month == search_date.month) &
+                                 (Entry.timestamp.day == search_date.day))
 
     elif search_date_range:
         start_date, end_date = search_date_range
@@ -112,15 +114,18 @@ def view_entries(search_name=None,
         term = "%"+ search_term +"%"
         entries = entries.where(Entry.note ** term | Entry.task_title ** term)
 
-    display_entry(entries)
+    # convert to a list of id
+    entries_ids = [entry.id for entry in entries]
+    display_entry(entries_ids)
 
 
-def display_entry(entries):
+def display_entry(entries_ids):
 
-    if entries:
+    if entries_ids:
         i = 0
-        while i < len(entries):
-            entry = entries[i]
+        while i < len(entries_ids):
+            entry_id = entries_ids[i]
+            entry = Entry.get(Entry.id == entry_id)
             timestamp = entry.timestamp.strftime("%A %B %d, %Y %I:%M%p")
             clear()
             print(timestamp)
@@ -130,7 +135,7 @@ def display_entry(entries):
             print(f"TASK TITLE: {entry.task_title}")
             print(f"TIME SPENT (minutes): {entry.time_spent}")
             print(f"NOTE: \n{entry.note}")
-            print(f"\nEntry {i+1} of {len(entries)}")
+            print(f"\nEntry {i+1} of {len(entries_ids)}")
             print("=" * len(timestamp))
             print("n) next entry")
             print("p) previous entry")
@@ -143,13 +148,14 @@ def display_entry(entries):
                 break
             elif next_action == "e":
                 edit_entry(entry)
-                view_entries()
-                break
+
             elif next_action == "d":
                 delete_entry(entry)
+                entries_ids.remove(entry_id)
+
             elif (next_action == "p") & (i > 0):
                 i -= 1
-            elif (next_action == "n") & (i < len(entries)-1):
+            elif (next_action == "n") & (i < len(entries_ids)-1):
                 i += 1
     else:
         input("Entry is not found. Press enter to return to search menu. ")
@@ -197,54 +203,97 @@ def delete_entry(entry):
         entry.delete_instance()
         print("Entry deleted successfully!")
 
+
 def edit_entry(entry):
     """Edit an entry"""
 
     choice = None
-
     while choice != "q":
         clear()
         print("EDIT ENTRY")
         print("What field would you like to edit?")
         print("Press 'q' to return to the previous menu.")
-        print("a) Name")
-        print("b) Date")
-        print("c) Task title")
-        print("d) Time spent")
-        print("e) Note")
 
-        choice = input("Action: ").lower().strip()
+        for k, v in edit_menu.items():
+            print(f"{k}) {v.__doc__}")
 
-        if choice == "a":
-            clear()
-            name = input("Name : ")
-            entry.update(name = name).execute()
-            input("Name changed successfully! Press Enter to continue")
+        choice = input('Action: ').lower().strip()
+
+        if choice in edit_menu:
+            edit_menu[choice](entry)
 
 
 
-def get_date(msg):
+def edit_name(entry):
+    """Name"""
+    clear()
+    name = input("Name : ")
+    param = {'name': name}
+    update_entry(entry, param)
+
+def edit_date(entry):
+    """Date"""
+    clear()
+    timestamp = get_date("Edit Date", timestamp=True)
+    param = {"timestamp": timestamp}
+    update_entry(entry, param)
+
+def edit_task_title(entry):
+    """Task title"""
+    clear()
+    task_title = input("Task title: ")
+    param = {"task_title": task_title}
+    update_entry(entry, param)
+
+def edit_time_spent(entry):
+    """Time spent"""
+    clear()
+    time_spent = get_int_number("Time Spent (rounded minutes): ")
+    param = {"time_spent": time_spent}
+    update_entry(entry, param)
+
+def edit_note(entry):
+    """Note"""
+    clear()
+    print("Note (press ctr+d when finished) : ")
+    note = sys.stdin.read().strip()
+    param = {"note": note}
+    update_entry(entry, param)
+
+def update_entry(entry, param_dict):
+    entry.update(**param_dict).execute()
+    fields = [ k for k, _ in param_dict.items()]
+    input(f"\nLog field of '{', '.join(fields)}' edited successfully! Press Enter to continue")
+
+def get_date(msg, timestamp=False):
     """
     Get user input date
     :param msg: string, additional msg to display to user
+    :param timestamp: boolean, get date and time
     :return: datetime object
     """
-    msg += "\nPlease use DD/MM/YYYY format: "
+    if timestamp:
+        msg += "\nPlease use 'DD/MM/YYYY HH:MM' (24 hour) format: "
+        fmt = "%d/%m/%Y %H:%M"
+    else:
+        msg += "\nPlease use 'DD/MM/YYYY' format: "
+        fmt = "%d/%m/%Y"
 
     while True:
         input_date = input(msg)
         try:
-            date = datetime.datetime.strptime(input_date, "%d/%m/%Y")
+            date = datetime.datetime.strptime(input_date, fmt)
         except ValueError:
-            print(f"{input_date} doesn't seem to be a valid date.")
+            print(f"{input_date} doesn't seem to be a valid date or format.")
         else:
             return date
 
 
 def get_int_number(msg):
     """Get user input integer number"""
+    clear()
     while True:
-        clear()
+
         user_input = input(msg)
 
         try:
@@ -268,6 +317,14 @@ search_menu = OrderedDict([
     ('3', search_by_name),
     ('4', search_by_term),
     ('5', search_by_time_spent)
+])
+
+edit_menu = OrderedDict([
+    ("a", edit_name),
+    ("b", edit_date),
+    ("c", edit_task_title),
+    ("d", edit_time_spent),
+    ("e", edit_note)
 ])
 
 if __name__ == "__main__":
